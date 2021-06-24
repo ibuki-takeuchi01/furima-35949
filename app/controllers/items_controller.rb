a85f7463435f90c29628a5212a2743398b004db9
ibuki-takeuchi01 <tomica.0324@gmail.com>
2021-06-24 16:18:44 +0900

    未ログイン時の出品ボタンのパス設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   
   def index
     #@items = Item.includes(:user)
@@ -22,12 +22,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :product_name, :description, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :day_to_ship_id, :price).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to user_session_path
-    end
   end
 
 end
