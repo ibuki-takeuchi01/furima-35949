567d106cb5cb9707eda5e83fbce0b60c548a3f15
ibuki-takeuchi01 <tomica.0324@gmail.com>
2021-07-02 16:47:41 +0900

    レビュー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  before_action :set_item, only: [:index,:create,:pay_item]
   
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderShippingAddress.new
       if current_user == @item.user
       redirect_to root_path
       end
   end
 
-  def new
-  end
-
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderShippingAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -30,13 +26,16 @@ end
     end
 
     def pay_item
-      @item = Item.find(params[:item_id])
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
       Payjp::Charge.create(
         amount: @item.price,  # 商品の値段
         card: order_params[:token],    # カードトークン
         currency: 'jpy'                 # 通貨の種類（日本円）
       )
+    end
+
+    def set_item
+      @item = Item.find(params[:id])
     end
 
 end
--- spec/models/order_shipping_address_spec.rb
+++ spec/models/order_shipping_address_spec.rb
@@ -73,8 +73,13 @@ end
     @order_address.valid?
     expect(@order_address.errors.full_messages).to include("Phone number is invalid")
   end
-  it 'phone_numberが10桁or11桁で無ければば登録できない' do
+  it 'phone_numberが9桁以下では登録できない' do
     @order_address.phone_number = '000111222'
+    @order_address.valid?
+    expect(@order_address.errors.full_messages).to include("Phone number is invalid")
+  end
+  it 'phone_numberが12桁以上では登録できない' do
+    @order_address.phone_number = '000011112222'
     @order_address.valid?
     expect(@order_address.errors.full_messages).to include("Phone number is invalid")
   end
