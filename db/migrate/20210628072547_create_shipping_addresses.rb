c0550e240b95b923e4d6e233ec21f317a8e0798c
ibuki-takeuchi01 <tomica.0324@gmail.com>
2021-07-05 11:39:23 +0900

    デプロイ修正

--- db/migrate/20210628072547_create_shipping_addresses.rb
+++ db/migrate/20210628072547_create_shipping_addresses.rb
@@ -5,7 +5,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.0]
       t.integer :prefecture_id ,    null: false 
       t.string :city_name,          null: false
       t.string :address,            null: false
-      t.string :building_name,
+      t.string :building_name
       t.integer :phone_number,      null: false
       t.references :order,          foreign_key: true
       t.timestamps
